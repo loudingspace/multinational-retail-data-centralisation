0e4cdc8e3ec76017d0894fa34816752b9aa71fe0
loudingspace <122986737+loudingspace@users.noreply.github.com>
2023-10-28 18:14:41 +0100

    We now initialise connection to postgres database when DatabaseConnector is initialised

--- classes/database_utils.py
+++ classes/database_utils.py
@@ -12,6 +12,20 @@ class DatabaseConnector:
     DATABASE_TYPE = 'postgresql'
     DBAPI = 'psycopg2'
 
+    def __init__(self):
+        try:
+            with open('../info/postgresdb_creds.yaml') as file:
+                db_creds = yaml.safe_load(file)
+                # print(db_creds)
+
+        except FileNotFoundError:
+            print('Sorry, the db config file is not currently available.')
+
+        HOST, USER, PASSWORD, DATABASE, PORT = db_creds.values()
+
+        self.engine = create_engine(
+            f"{DatabaseConnector.DATABASE_TYPE}+{DatabaseConnector.DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
+
     def read_db_creds(self) -> dict:  # THIS SHOULD BE PRIVATE
         '''read the credentials yaml file and return a dictionary of the credentials
 
@@ -54,22 +68,6 @@ class DatabaseConnector:
             # print('Table name: ', table_names)
             return table_names
 
-        # DATABASE_TYPE = 'postgresql'
-        # DBAPI = 'psycopg2'
-        # HOST = '127.0.0.1'
-        # USER = 'postgres'
-        # PASSWORD = 'ronald22'
-        # DATABASE = 'pagila'
-        # PORT = 5432
-        # engine = create_engine(
-        #     f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
-
-        # RDS_HOST: data-handling-project-readonly.cq2e8zno855e.eu-west-1.rds.amazonaws.com
-        # RDS_PASSWORD: AiCore2022
-        # RDS_USER: aicore_admin
-        # RDS_DATABASE: postgres
-        # RDS_PORT: 5432
-
         # engine.execution_options(isolation_level='AUTOCOMMIT').connect()
         # from sqlalchemy import inspect
         # inspector = inspect(engine)
@@ -86,37 +84,14 @@ class DatabaseConnector:
         Takes a dataframe and converts this to SQL and then uploads it to the database with the table_name
         '''
 
-        try:
-            with open('../info/postgresdb_creds.yaml') as file:
-                db_creds = yaml.safe_load(file)
-                # print(db_creds)
-
-        except FileNotFoundError:
-            print('Sorry, the db config file is not currently available.')
-
-        # # TODO: Need to change this to make it more secure
-        # #  this needs tidying up
-        # HOST = '127.0.0.1'
-        # USER = 'postgres'
-        # PASSWORD = 'ronald22'
-        # DATABASE = 'sales_data'
-        # PORT = 5432
-
-        # print('db_cred: ', type(db_creds))
-
-        # um i thought this was a dictionary. order appears to matter here, so i think it's a list
-        HOST, USER, PASSWORD, DATABASE, PORT = db_creds.values()
-
-        engine = create_engine(
-            f"{DatabaseConnector.DATABASE_TYPE}+{DatabaseConnector.DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
-
-        with engine.connect() as connection:
+        with self.engine.connect() as connection:
             # result = connection.execute(
             #     text("SELECT * FROM information_schema.schemata"))
             # for row in result:
             #     print(row)
 
-            df.to_sql(table_name, engine, if_exists='replace', index=False)
+            df.to_sql(table_name, self.engine,
+                      if_exists='replace', index=False)
 
 
 #######
